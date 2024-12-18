7caf32b764ab1241dba08faf03d21ac1ca482509
Bhavin B <bhavinbhadani@yahoo.in>
2024-12-18 10:35:55 +0530

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Bhavin Bhadani on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
